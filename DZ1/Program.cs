5490efe8a577fdd6cf41ff20cc9cbdf3e0668573
Valentina Korotkova <korobok.valek@mail.ru>
2023-05-27 15:21:41 +0600

    Прикрепление Домашней работы 1

---
+++ DZ1/Program.cs
@@ -0,0 +1,69 @@
+﻿/*адача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число болешее, а какое меньшее.*/
+
+/*Console.WriteLine("Введите два числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if(a==b){
+    Console.WriteLine("Числа равны");
+}
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("max="+max+"");
+Console.WriteLine("min="+min+"");*/
+
+/* Задача 4: Напишите программу, которая на вход принимает три числа и выдаёт максимальное из этих чисел.*/
+/*Console.WriteLine("Введите три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int max = 0;
+if(a>max)
+{
+    max=a;
+}
+if(b>max)
+{
+    max=b;
+}
+if(c>max)
+{
+    max=c;
+}
+Console.WriteLine("max="+max);*/
+/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли оно четным(делится ли оно наидва без остатка)*/
+/*Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int rezult = a%2;
+if(rezult == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+Console.WriteLine("Число является НЕчетным");
+}*/
+
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+/*Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int firstZnachenie = 2;
+if(a>1)
+{
+    while(firstZnachenie<=a)
+    {
+Console.Write(firstZnachenie + " ");
+firstZnachenie = firstZnachenie + 2;
+    } 
+}
+*/
+
